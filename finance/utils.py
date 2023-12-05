3bf3f12e0f06c01e01f5a08e5f4d1747cfdc60b8
Sahil Singhai Jain <ssjain@lumenore.com>
2023-12-05 18:49:38 +0530

    build fix

--- finance/utils.py
+++ finance/utils.py
@@ -1,6 +1,5 @@
 from djangoproject.constants import Constants
 from rest_framework.status import HTTP_200_OK
-import MySQLdb
 
 
 constants = Constants()
@@ -12,8 +11,8 @@ def create_response(data, code =HTTP_200_OK, error=False, **kwags):
     constants.STATUS200["data"] = data
 
 
-def get_connection(org='financeApp'):
-    connection = MySQLdb.connect(
-        **constants.get_conn_info(org=org)
-    )
-    return connection
+# def get_connection(org='financeApp'):
+#     connection = MySQLdb.connect(
+#         **constants.get_conn_info(org=org)
+#     )
+#     return connection
