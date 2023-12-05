75fd5dad14a13b55ab99c62dcdbcc7261fb86a4b
Sahil Singhai Jain <ssjain@lumenore.com>
2023-12-05 18:50:19 +0530

    build fix

--- finance/views.py
+++ finance/views.py
@@ -5,7 +5,7 @@ from rest_framework.status import (
     HTTP_500_INTERNAL_SERVER_ERROR,
 )
 from rest_framework.views import APIView
-from .utils import create_response, get_connection
+from .utils import create_response
 from djangoproject.constants import Constants
 from djangoproject.main_logger import set_up_logging
 from threading import Thread
