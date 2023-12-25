e23afe2e8e38e3a3f131f4b30eef5c7f2527c458
Sahil Singhai Jain <ssjain@lumenore.com>
2023-12-25 22:36:20 +0530

    Ecape 0

--- finance/utils.py
+++ finance/utils.py
@@ -67,9 +67,9 @@ def create_filter(datalist):
 
     for row in datalist:
         filters = {}
-        if row.get("changePrecentage"):
+        if row.get("changePrecentage") is not None:
             change_value = {"change_value": (row["changePrecentage"] / 100) + 1}
-        elif row.get("changeValue"):
+        elif row.get("changeValue") is not None:
             change_value = {
                 "change_value": row["changeValue"],
                 "date": row["date"],
